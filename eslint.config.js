47f0dea9b8741b1e441d52b620542a63504e55b9
GTPSHAX <rafiehasannudin@gmail.com>
2025-12-26 08:37:40 +0700

    feat: add ESLint configuration for TypeScript support

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        fetch: 'readonly',
+        RequestInit: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      // Add custom rules here
+    },
+  },
+  {
+    ignores: ['dist/', 'tests/', 'node_modules/'],
+  },
+];
